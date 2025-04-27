a86346bcb0033448f67d159f932c7ee3b9770559
Tom Gundersen <teg@jklm.no>
2025-04-27 02:21:21 +0100

    Complete Phase 1.3: Data Fetching Layer

--- infra/lib/lkml-assistant-stack.ts
+++ infra/lib/lkml-assistant-stack.ts
@@ -4,11 +4,15 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as events from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
 
 export class LkmlAssistantStack extends cdk.Stack {
   // Make tables public so they can be accessed by other constructs
   public readonly patchesTable: dynamodb.Table;
   public readonly discussionsTable: dynamodb.Table;
+  public readonly fetchPatchesLambda: lambda.Function;
+  public readonly fetchDiscussionsLambda: lambda.Function;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -75,6 +79,69 @@ export class LkmlAssistantStack extends cdk.Stack {
       projectionType: dynamodb.ProjectionType.ALL,
     });
 
-    // TODO: Define Lambda functions, EventBridge rules, and other resources
+    // Define Lambda functions
+    
+    // 1. Fetch Patches Lambda
+    this.fetchPatchesLambda = new lambda.Function(this, 'FetchPatchesFunction', {
+      functionName: 'LkmlAssistant-FetchPatches',
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../src/functions/fetch-patches')),
+      timeout: cdk.Duration.seconds(300),
+      memorySize: 512,
+      environment: {
+        PATCHES_TABLE_NAME: this.patchesTable.tableName,
+        FETCH_DISCUSSIONS_LAMBDA: 'LkmlAssistant-FetchDiscussions',
+      },
+    });
+    
+    // Grant DynamoDB permissions to FetchPatches Lambda
+    this.patchesTable.grantWriteData(this.fetchPatchesLambda);
+    
+    // Grant Lambda permissions to invoke other Lambdas
+    this.fetchPatchesLambda.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['lambda:InvokeFunction'],
+      resources: ['*'],  // In a production environment, this should be restricted to specific Lambda ARNs
+    }));
+    
+    // 2. Fetch Discussions Lambda
+    this.fetchDiscussionsLambda = new lambda.Function(this, 'FetchDiscussionsFunction', {
+      functionName: 'LkmlAssistant-FetchDiscussions',
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../src/functions/fetch-discussions')),
+      timeout: cdk.Duration.seconds(300),
+      memorySize: 512,
+      environment: {
+        DISCUSSIONS_TABLE_NAME: this.discussionsTable.tableName,
+        PATCHES_TABLE_NAME: this.patchesTable.tableName,
+      },
+    });
+    
+    // Grant DynamoDB permissions to FetchDiscussions Lambda
+    this.discussionsTable.grantWriteData(this.fetchDiscussionsLambda);
+    this.patchesTable.grantReadWriteData(this.fetchDiscussionsLambda);
+    
+    // Set up EventBridge rules for scheduling
+    
+    // Schedule FetchPatches Lambda to run hourly
+    const fetchPatchesRule = new events.Rule(this, 'FetchPatchesRule', {
+      ruleName: 'LkmlAssistant-FetchPatchesHourly',
+      schedule: events.Schedule.rate(cdk.Duration.hours(1)),
+      description: 'Fetch patches from Patchwork API hourly',
+    });
+    
+    fetchPatchesRule.addTarget(new targets.LambdaFunction(this.fetchPatchesLambda, {
+      event: events.RuleTargetInput.fromObject({
+        page: 1,
+        per_page: 50,
+        process_all_pages: true
+      })
+    }));
+    
+    // Note: FetchDiscussions Lambda will be triggered by the FetchPatches Lambda
+    // for each patch, so we don't need to schedule it directly.
+    
+    // We'll add step functions or direct invocation in Phase 2
   }
 }
